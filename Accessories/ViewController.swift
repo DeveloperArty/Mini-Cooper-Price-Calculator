687555f0aaa090b4130970930decc0edda0399d2
DeveloperArty <apple_dev.arty@mail.ru>
2016-11-30 23:25:29 +0300

    UI upgraded, switches outlets added

--- Accessories/ViewController.swift
+++ Accessories/ViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var wiredPackSwitch: UISwitch!
+    @IBOutlet weak var electricBlueColorSwitch: UISwitch!
+    @IBOutlet weak var vanitySpokeDCWheelsSwitch: UISwitch!
+    @IBOutlet weak var blackNWitheLinesSwitch: UISwitch!
+    @IBOutlet weak var calculateButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUIElements()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,7 +27,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func setupUIElements() {
+        calculateButton.layer.cornerRadius = 12
+        calculateButton.layer.borderWidth = 2
+        calculateButton.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    
 }
 
