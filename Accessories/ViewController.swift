19b63938e1f1f9a341cd7105536908f7f757ea3a
DeveloperArty <apple_dev.arty@mail.ru>
2016-11-30 23:59:51 +0300

    alert controller added

--- Accessories/ViewController.swift
+++ Accessories/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var vanitySpokeDCWheelsSwitch: UISwitch!
     @IBOutlet weak var blackNWitheLinesSwitch: UISwitch!
     @IBOutlet weak var calculateButton: UIButton!
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,6 +33,14 @@ class ViewController: UIViewController {
         calculateButton.layer.cornerRadius = 12
         calculateButton.layer.borderWidth = 2
         calculateButton.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        
+        let alert = UIAlertController(title: "Special Offer!", message: "Only today You will have an opportunity to purchase this car at a special price ", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Yep, call me back!", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
     
---
+++ Accessories/calculatesModel.swift
@@ -0,0 +1,10 @@
+//
+//  calculatesModel.swift
+//  Accessories
+//
+//  Created by Artem Pavlov on 30.11.16.
+//  Copyright © 2016 Arty. All rights reserved.
+//
+
+import Foundation
+
