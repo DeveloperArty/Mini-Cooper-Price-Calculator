ae6a28385ef85d09be1d72c451937ec401333bd9
DeveloperArty <apple_dev.arty@mail.ru>
2016-11-30 21:51:12 +0300

    Initial Commit

---
+++ Accessories/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Accessories
+//
+//  Created by Artem Pavlov on 30.11.16.
+//  Copyright © 2016 Arty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
