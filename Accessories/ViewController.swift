44840c6fcd13a4d7d877c567cc86d3d875a8407d
DeveloperArty <apple_dev.arty@mail.ru>
2016-12-02 23:40:25 +0300

    One View Controller commit

--- Accessories/ViewController.swift
+++ Accessories/ViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var calculatesView: UIView!
     @IBOutlet weak var wiredPackSwitch: UISwitch!
     @IBOutlet weak var electricBlueColorSwitch: UISwitch!
     @IBOutlet weak var vanitySpokeDCWheelsSwitch: UISwitch!
     @IBOutlet weak var blackNWitheLinesSwitch: UISwitch!
     @IBOutlet weak var calculateButton: UIButton!
     
+    //MARK: - Standart functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,18 +31,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK: - Additional UI Setup
+    
     func setupUIElements() {
+        
+        calculatesView.isHidden = true
+        calculatesView.layer.cornerRadius = 5
+        calculatesView.layer.borderColor = UIColor.cyan.cgColor
+        calculatesView.layer.borderWidth = 2
+        
         calculateButton.layer.cornerRadius = 12
         calculateButton.layer.borderWidth = 2
         calculateButton.layer.borderColor = UIColor.lightGray.cgColor
+        
+        wiredPackSwitch.isOn = false
+        electricBlueColorSwitch.isOn = false
+        vanitySpokeDCWheelsSwitch.isOn = false
+        blackNWitheLinesSwitch.isOn = false
     }
     
+    //MARK: - UI Events
+    
     @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        calculatesView.isHidden = false
         
-        let alert = UIAlertController(title: "Special Offer!", message: "Only today You will have an opportunity to purchase this car at a special price ", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Yep, call me back!", style: .default, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
     }
     
     
--- Accessories/calculatesModel.swift
+++ Accessories/calculatesModel.swift
@@ -6,5 +6,31 @@
 //  Copyright © 2016 Arty. All rights reserved.
 //
 
-import Foundation
 
+struct Calculates {
+    
+    let basicCarPrice: Int = 1499000
+    let wiredPackPrice: Int = 155000
+    let electricBlueColorPrice: Int = 30000
+    let vanitySpokeWheelsPrice: Int = 96500
+    let linesPrice: Int = 7800
+    
+    func calculatePrice(wiredSwitchStatus: Bool, colorSwitchStatus: Bool, wheelsSwitchStatus: Bool, linesSwitchStatus: Bool) -> Int {
+        var finalPrice = basicCarPrice
+        if wiredSwitchStatus == true {
+            finalPrice += wiredPackPrice
+        }
+        if colorSwitchStatus == true {
+                finalPrice += electricBlueColorPrice
+            }
+        if wheelsSwitchStatus == true {
+            finalPrice += vanitySpokeWheelsPrice
+        }
+        if linesSwitchStatus == true {
+            finalPrice += linesPrice
+        }
+        
+        return finalPrice
+    }
+    
+}
